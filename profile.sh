cacf3df9dac452989a7e90c7ebdf25af27cd5e36
Matthew O'Connell <Matthew.D.OConnell>
2021-09-20 14:03:21 -0400

    To use auto-generated names in profile

--- profile.sh
+++ profile.sh
@@ -2,5 +2,5 @@
 module load  cuda_11.2.2 gcc_9.2.0   clang_12.0.0   
 module list
 export KOKKOS_PROFILE_LIBRARY=/lustre2/hpnobackup1/mdoconn1/kokkos-tools/kp_nvprof_connector.so
-nsys profile --force-overwrite=true -o out.profile -t cuda,nvtx ./build-nvcc/vul/ucgo -g 10 10 100
+nsys profile --force-overwrite=true -t cuda,nvtx ./build-nvcc/vul/ucgo -g 100 100 100
 #./build-gpu/vul/ucgo
